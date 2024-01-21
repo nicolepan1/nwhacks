425cf6b1ad7db68607de33adfc6f98bf2c3cbd65
radhikab1 <50178604+radhikab1@users.noreply.github.com>
2024-01-21 11:52:40 -0800

    edited frontend and backend sms scheduling

--- backend/send_sms.js
+++ backend/send_sms.js
@@ -6,14 +6,14 @@ require('dotenv').config()
 const accountSid = process.env.TWILIO_TOKEN;
 const authToken = process.env.TWILIO_ACCOUNT_SID;
 
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken, phone_number);
 
 client.messages
   .create({
-     body: 'This is a reminder to take your medication, medication_name with a dosage of medication_dosage!',
+     body: 'This is a reminder to take your Lipitor medication with a dosage of 20mg!',
      messagingServiceSid: 'MG0277b73b3d0af82d0a8c4cb91180f0e1',
-     sendAt: new Date(Date.UTC(2023, 12, 21, 19, 30, 10)),
+     sendAt: new Date(Date.UTC(2023, 12, 21, 19, 49, 0)),
      scheduleType: 'fixed',
-     to: '+17789289556'
+     to: '${phone_number}'
    })
   .then(message => console.log(message.sid));
