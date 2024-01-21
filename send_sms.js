e4d327d859e64b2b716dcf53f81b3f80786bbb33
radhikab1 <50178604+radhikab1@users.noreply.github.com>
2024-01-21 11:07:03 -0800

    changed to scheduling sms

--- send_sms.js
+++ send_sms.js
@@ -11,7 +11,9 @@ const client = require('twilio')(accountSid, authToken);
 client.messages
   .create({
      body: 'This is a reminder to take your medication, medication_name with a dosage of medication_dosage!',
-     from: '+13612736216',
+     messagingServiceSid: 'MG0277b73b3d0af82d0a8c4cb91180f0e1',
+     sendAt: new Date(Date.UTC(2024, 1, 21, 19, 12, 10)),
+     scheduleType: 'fixed',
      to: '+17789289556'
    })
   .then(message => console.log(message.sid));
